54c45091b743caeaec4d5eac987c0fc6aa7aaa07
imjg46 <67748736+imjg46@users.noreply.github.com>
2026-07-01 00:04:59 +0000

    test

--- lib/services/history_service.dart
+++ lib/services/history_service.dart
@@ -7,44 +7,44 @@ import '../models/payment_history.dart';
 class HistoryService {
   static const String _storageKey = "support_history";
 
-    Future<List<PaymentHistory>> loadHistory() async {
-        final prefs = await SharedPreferences.getInstance();
+  Future<List<PaymentHistory>> loadHistory() async {
+    final prefs = await SharedPreferences.getInstance();
 
-            final data = prefs.getStringList(_storageKey) ?? [];
+    final data = prefs.getStringList(_storageKey) ?? [];
 
-                return data
-                        .map(
-                                  (e) => PaymentHistory.fromJson(
-                                              jsonDecode(e),
-                                                        ),
-                                                                )
-                                                                        .toList();
-                                                                          }
+    return data
+        .map(
+          (e) => PaymentHistory.fromJson(
+            jsonDecode(e),
+          ),
+        )
+        .toList();
+  }
 
-                                                                            Future<void> addHistory(int amount) async {
-                                                                                final prefs = await SharedPreferences.getInstance();
+  Future<void> addHistory(int amount) async {
+    final prefs = await SharedPreferences.getInstance();
 
-                                                                                    final data = prefs.getStringList(_storageKey) ?? [];
+    final data = prefs.getStringList(_storageKey) ?? [];
 
-                                                                                        final history = PaymentHistory(
-                                                                                              amount: amount,
-                                                                                                    date: DateTime.now(),
-                                                                                                        );
+    final history = PaymentHistory(
+      amount: amount,
+      date: DateTime.now(),
+    );
 
-                                                                                                            data.insert(
-                                                                                                                  0,
-                                                                                                                        jsonEncode(history.toJson()),
-                                                                                                                            );
+    data.insert(
+      0,
+      jsonEncode(history.toJson()),
+    );
 
-                                                                                                                                await prefs.setStringList(
-                                                                                                                                      _storageKey,
-                                                                                                                                            data,
-                                                                                                                                                );
-                                                                                                                                                  }
+    await prefs.setStringList(
+      _storageKey,
+      data,
+    );
+  }
 
-                                                                                                                                                    Future<void> clearHistory() async {
-                                                                                                                                                        final prefs = await SharedPreferences.getInstance();
+  Future<void> clearHistory() async {
+    final prefs = await SharedPreferences.getInstance();
 
-                                                                                                                                                            await prefs.remove(_storageKey);
-                                                                                                                                                        }
-                                }
+    await prefs.remove(_storageKey);
+  }
+}
